b9cf5c199f43ec7c424678cd27d73a1464f38342
Thomas <t.jakubowski@eleve.leschartreux.net>
2022-06-08 23:23:31 +0200

    fix redirection bug and add verification

---
+++ App/Views/LogOut.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Modal, ScrollView } from "react-native";
+import { Button } from "@react-native-material/core";
+import OwnTextInput from "../components/TextInput";
+import axios from "axios";
+import { BASE_API } from "@env";
+import DisplayQuizzes from "../components/DisplayQuizzes";
+import ConfimationModal from "../components/ConfimationModal";
+
+const LogOut = ({ navigation }) => {
+}
+export default LogOut;
