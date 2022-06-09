ad2c5c16f8ec7a2c5b98f31f607d995930ca752e
Thomas <t.jakubowski@eleve.leschartreux.net>
2022-06-09 03:50:51 +0200

    design modal

--- App/components/ConfimationModal.js
+++ App/components/ConfimationModal.js
@@ -15,8 +15,8 @@ export default ConfirmationModal = ({
         <View style={[style, styles.container]}>
           <Text>{text}</Text>
           <View style={styles.row}>
-            <Button variant="outlined" title="Non" onPress={() => onPressNo()} />
-            <Button variant="outlined" title="Oui" onPress={() => onPressYes()} />
+            <Button style={styles.button} variant="outlined" title="Non" onPress={() => onPressNo()} />
+            <Button style={styles.button} variant="outlined" title="Oui" onPress={() => onPressYes()} />
           </View>
         </View>
       ) : null}
@@ -26,15 +26,21 @@ export default ConfirmationModal = ({
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "grey",
+    backgroundColor: "white",
     position: "absolute",
     justifyContent: "center",
+    marginLeft: "20%",
+    marginTop: "50%",
     alignItems: "center",
-    marginHorizontal: "30%",
-    marginVertical: "50%",
+    width: 200,
+    height: 100,
   },
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
+  button: {
+    marginHorizontal: 10,
+    marginTop: 16
+  }
 });
